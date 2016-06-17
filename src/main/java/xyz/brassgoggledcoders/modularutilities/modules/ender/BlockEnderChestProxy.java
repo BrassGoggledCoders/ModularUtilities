a8e508c671bb8a78428cd8e488e81f103e3203f9
warlordjones <warlordjones@gmail.com>
2016-06-17 23:04:47 +0100

    You can now right click the Ender Crate to show who its linked to.

--- src/main/java/xyz/brassgoggledcoders/modularutilities/modules/ender/BlockEnderChestProxy.java
+++ src/main/java/xyz/brassgoggledcoders/modularutilities/modules/ender/BlockEnderChestProxy.java
@@ -1,14 +1,21 @@
 package xyz.brassgoggledcoders.modularutilities.modules.ender;
 
+import java.util.UUID;
+
+import javax.annotation.Nullable;
+
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.Container;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
+import net.minecraftforge.common.UsernameCache;
 import xyz.brassgoggledcoders.boilerplate.blocks.BlockTEBase;
 
 public class BlockEnderChestProxy extends BlockTEBase
@@ -16,6 +23,24 @@ public class BlockEnderChestProxy extends BlockTEBase
 	public BlockEnderChestProxy()
 	{
 		super(Material.ROCK, "ender_proxy");
+	}
+
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
+			EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ)
+	{
+		if(!worldIn.isRemote)
+		{
+			TileEntityEnderChestProxy ex = (TileEntityEnderChestProxy) worldIn.getTileEntity(pos);
+			UUID placerID = ex.getPlacerUUID();
+			if(placerID != null)
+				playerIn.addChatMessage(
+						new TextComponentString("Linked Player is: " + UsernameCache.getMap().get(placerID)));
+			else
+				playerIn.addChatMessage(new TextComponentString("No linked player"));
+			return true;
+		}
+		return false;
 	}
 
 	@Override
@@ -40,8 +65,9 @@ public class BlockEnderChestProxy extends BlockTEBase
 	public int getComparatorInputOverride(IBlockState blockState, World worldIn, BlockPos pos)
 	{
 		// TODO
-		return Container.calcRedstoneFromInventory(
-				((TileEntityEnderChestProxy) worldIn.getTileEntity(pos)).getEnderInventory());
+		return 0;
+		// return Container.calcRedstoneFromInventory(
+		// ((TileEntityEnderChestProxy) worldIn.getTileEntity(pos)).getEnderInventory());
 	}
 
 	@Override
