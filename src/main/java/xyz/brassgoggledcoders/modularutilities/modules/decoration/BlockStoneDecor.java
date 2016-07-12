236035ec278ad33733ef81d83b326624e816b85e
warlordjones <warlordjones@gmail.com>
2016-07-12 22:14:46 +0100

    Elder Prismarine
    TODO: Recipes, Elder Sea Lantern

--- src/main/java/xyz/brassgoggledcoders/modularutilities/modules/decoration/BlockStoneDecor.java
+++ src/main/java/xyz/brassgoggledcoders/modularutilities/modules/decoration/BlockStoneDecor.java
@@ -53,7 +53,8 @@ public class BlockStoneDecor extends BlockSubBase implements ISimpleVariant {
 	}
 
 	public enum EnumBlockType implements IBlockType {
-		CLINKER_BRICK(0), CARVED_STONE(1);
+		CLINKER_BRICK(0), CARVED_STONE(1), ELDER_PRISMARINE_ROUGH(2), ELDER_PRISMARINE_BRICKS(3),
+		ELDER_PRISMARINE_DARK(4);
 
 		public static final EnumBlockType[] VALUES = values();
 
