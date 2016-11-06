ccf7a2e2da56a7a74679fee4dfc5c4353a36fae2
Skylar Sommers <ssommers@criticalmix.com>
2016-11-06 17:29:58 -0500

    Trying to fix model errors

--- src/main/java/xyz/brassgoggledcoders/modularutilities/modules/construction/ConstructionModule.java
+++ src/main/java/xyz/brassgoggledcoders/modularutilities/modules/construction/ConstructionModule.java
@@ -7,10 +7,16 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialLiquid;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.ItemMeshDefinition;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.ForgeModContainer;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
@@ -55,10 +61,8 @@ public class ConstructionModule extends ModuleBase {
 		concrete = new BlockBase(Material.ROCK, "concrete").setHardness(3).setResistance(18);
 		getBlockRegistry().register(concrete);
 
-		this.getMod().getModelLoader().registerFluidModel(filler_fluid_block,
-				new ResourceLocation(ModularUtilities.MODID + ":fluids", "filler_fluid"));
-		this.getMod().getModelLoader().registerFluidModel(concrete_fluid_block,
-				new ResourceLocation(ModularUtilities.MODID + ":fluids", "concrete_fluid"));
+		registerFluidBlockRendering(concrete_fluid, concrete_fluid.getName());
+		registerFluidBlockRendering(filler_fluid, filler_fluid.getName());
 		/*
 		 * TODO:
 		 * - Quickdry Concrete
@@ -70,6 +74,21 @@ public class ConstructionModule extends ModuleBase {
 		 * - Scaffolding
 		 * - Blast resistant door, pushable blast blocks.
 		 */
+	}
+
+	public void registerFluidBlockRendering(Fluid fluid, String name) {
+
+		FluidStateMapper mapper = new FluidStateMapper(fluid, name);
+		Block block = fluid.getBlock();
+		Item item = Item.getItemFromBlock(block);
+
+		// item-model
+		if (item != null) {
+			ModelLoader.registerItemVariants(item);
+			ModelLoader.setCustomMeshDefinition(item, mapper);
+		}
+		// block-model
+		ModelLoader.setCustomStateMapper(block, mapper);
 	}
 
 	@Override
@@ -95,6 +114,32 @@ public class ConstructionModule extends ModuleBase {
 		@Override
 		public boolean blocksMovement() {
 			return true;
+		}
+	}
+
+	public static class FluidStateMapper extends StateMapperBase implements ItemMeshDefinition {
+
+		public final Fluid fluid;
+		public final ModelResourceLocation location;
+
+		public FluidStateMapper(Fluid fluid, String name) {
+			this.fluid = fluid;
+			location = new ModelResourceLocation(ModularUtilities.MODID + ":fluids", name);
+		}
+
+		public FluidStateMapper(Fluid fluid) {
+			this(fluid, fluid.getName());
+		}
+
+
+		@Override
+		protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
+			return location;
+		}
+
+		@Override
+		public ModelResourceLocation getModelLocation(ItemStack stack) {
+			return location;
 		}
 	}
 }
