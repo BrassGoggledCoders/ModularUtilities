e7980bc1489192eb7b69a2ee9611a1493cc70917
warlordjones <warlordjones@gmail.com>
2017-07-31 17:24:28 +0100

    Reenable baubles

--- src/main/java/xyz/brassgoggledcoders/modularutilities/modules/baubles/ItemBloodboundRing.java
+++ src/main/java/xyz/brassgoggledcoders/modularutilities/modules/baubles/ItemBloodboundRing.java
@@ -1,52 +1,53 @@
-// package xyz.brassgoggledcoders.modularutilities.modules.baubles;
-//
-// import baubles.api.BaubleType;
-// import com.teamacronymcoders.base.items.IHasRecipe;
-// import com.teamacronymcoders.base.items.ItemBaubleBase;
-// import net.minecraft.entity.EntityLivingBase;
-// import net.minecraft.init.Items;
-// import net.minecraft.item.ItemStack;
-// import net.minecraft.item.crafting.IRecipe;
-// import net.minecraft.potion.Potion;
-// import net.minecraft.potion.PotionEffect;
-// import net.minecraftforge.fml.common.Optional;
-// import net.minecraftforge.oredict.ShapedOreRecipe;
-//
-// import java.util.List;
-//
-// public class ItemBloodboundRing extends ItemBaubleBase implements IHasRecipe {
-//
-// public ItemBloodboundRing() {
-// super("bloodbound_ring");
-// this.setMaxStackSize(1);
-// }
-//
-// @Override
-// @Optional.Method(modid = "Baubles")
-// public BaubleType getBaubleType(ItemStack arg0) {
-// return BaubleType.RING;
-// }
-//
-// @Override
-// @Optional.Method(modid = "Baubles")
-// public boolean canUnequip(ItemStack arg0, EntityLivingBase arg1) {
-// return false;
-// }
-//
-// @Override
-// @Optional.Method(modid = "Baubles")
-// public void onWornTick(ItemStack stack, EntityLivingBase living) {
-// if(living.getHealth() == living.getMaxHealth()) {
-// living.setHealth(living.getMaxHealth() * 0.75F);
-// living.addPotionEffect(
-// new PotionEffect(Potion.getPotionFromResourceLocation("strength"), 120, 2, false, false));
-// }
-// }
-//
-// @Override
-// public List<IRecipe> getRecipes(List<IRecipe> recipes) {
-// recipes.add(new ShapedOreRecipe(new ItemStack(this), "IBI", "I I", "III", 'I', "ingotIron", 'B',
-// Items.BLAZE_POWDER));
-// return recipes;
-// }
-// }
+package xyz.brassgoggledcoders.modularutilities.modules.baubles;
+
+import java.util.List;
+
+import com.teamacronymcoders.base.items.IHasRecipe;
+import com.teamacronymcoders.base.items.ItemBaubleBase;
+
+import baubles.api.BaubleType;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraftforge.fml.common.Optional;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+
+public class ItemBloodboundRing extends ItemBaubleBase implements IHasRecipe {
+
+	public ItemBloodboundRing() {
+		super("bloodbound_ring");
+		this.setMaxStackSize(1);
+	}
+
+	@Override
+	@Optional.Method(modid = "Baubles")
+	public BaubleType getBaubleType(ItemStack arg0) {
+		return BaubleType.RING;
+	}
+
+	@Override
+	@Optional.Method(modid = "Baubles")
+	public boolean canUnequip(ItemStack arg0, EntityLivingBase arg1) {
+		return false;
+	}
+
+	@Override
+	@Optional.Method(modid = "Baubles")
+	public void onWornTick(ItemStack stack, EntityLivingBase living) {
+		if(living.getHealth() == living.getMaxHealth()) {
+			living.setHealth(living.getMaxHealth() * 0.75F);
+			living.addPotionEffect(
+					new PotionEffect(Potion.getPotionFromResourceLocation("strength"), 120, 2, false, false));
+		}
+	}
+
+	@Override
+	public List<IRecipe> getRecipes(List<IRecipe> recipes) {
+		recipes.add(new ShapedOreRecipe(new ItemStack(this), "IBI", "I I", "III", 'I', "ingotIron", 'B',
+				Items.BLAZE_POWDER));
+		return recipes;
+	}
+}
--- src/main/java/xyz/brassgoggledcoders/modularutilities/modules/baubles/ItemFireCharm.java
+++ src/main/java/xyz/brassgoggledcoders/modularutilities/modules/baubles/ItemFireCharm.java
@@ -1,53 +1,54 @@
-// package xyz.brassgoggledcoders.modularutilities.modules.baubles;
-//
-// import baubles.api.BaubleType;
-// import com.teamacronymcoders.base.items.IHasRecipe;
-// import com.teamacronymcoders.base.items.ItemBaubleBase;
-// import net.minecraft.entity.EntityLivingBase;
-// import net.minecraft.entity.player.EntityPlayer;
-// import net.minecraft.init.Blocks;
-// import net.minecraft.init.Items;
-// import net.minecraft.item.ItemStack;
-// import net.minecraft.item.crafting.IRecipe;
-// import net.minecraftforge.fml.common.Optional;
-// import net.minecraftforge.oredict.ShapedOreRecipe;
-//
-// import java.util.List;
-//
-// public class ItemFireCharm extends ItemBaubleBase implements IHasRecipe {
-//
-// public ItemFireCharm() {
-// super("fire_charm");
-// }
-//
-// @Override
-// @Optional.Method(modid = "Baubles")
-// public BaubleType getBaubleType(ItemStack bauble) {
-// return BaubleType.AMULET;
-// }
-//
-// @Override
-// @Optional.Method(modid = "Baubles")
-// public void onWornTick(ItemStack stack, EntityLivingBase living) {
-// if(living instanceof EntityPlayer) {
-// EntityPlayer player = (EntityPlayer) living;
-// if(player.isBurning() && !(player.isInLava()) && !(player.isImmuneToFire())) {
-// player.extinguish();
-// // if(stack.attemptDamageItem(1, player.getRNG())) {
-// // PlayerHandler.getPlayerBaubles(player).setInventorySlotContents(0, null);
-// // player.setFire(5);
-// // player.getEntityWorld().playSound(player, player.getPosition(),
-// // SoundEvent.REGISTRY.getObject(new ResourceLocation("block.furnace.fire_crackle")),
-// // SoundCategory.AMBIENT, 1F, 1F);
-// // }
-// }
-// }
-// }
-//
-// @Override
-// public List<IRecipe> getRecipes(List<IRecipe> recipes) {
-// recipes.add(new ShapedOreRecipe(new ItemStack(this), "SSS", "MDM", "OOO", 'S', Items.STRING, 'M', Blocks.MAGMA,
-// 'D', Items.DIAMOND, 'O', Blocks.OBSIDIAN));
-// return recipes;
-// }
-// }
+package xyz.brassgoggledcoders.modularutilities.modules.baubles;
+
+import java.util.List;
+
+import com.teamacronymcoders.base.items.IHasRecipe;
+import com.teamacronymcoders.base.items.ItemBaubleBase;
+
+import baubles.api.BaubleType;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.fml.common.Optional;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+
+public class ItemFireCharm extends ItemBaubleBase implements IHasRecipe {
+
+	public ItemFireCharm() {
+		super("fire_charm");
+	}
+
+	@Override
+	@Optional.Method(modid = "Baubles")
+	public BaubleType getBaubleType(ItemStack bauble) {
+		return BaubleType.AMULET;
+	}
+
+	@Override
+	@Optional.Method(modid = "Baubles")
+	public void onWornTick(ItemStack stack, EntityLivingBase living) {
+		if(living instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) living;
+			if(player.isBurning() && !(player.isInLava()) && !(player.isImmuneToFire())) {
+				player.extinguish();
+				// if(stack.attemptDamageItem(1, player.getRNG())) {
+				// PlayerHandler.getPlayerBaubles(player).setInventorySlotContents(0, null);
+				// player.setFire(5);
+				// player.getEntityWorld().playSound(player, player.getPosition(),
+				// SoundEvent.REGISTRY.getObject(new ResourceLocation("block.furnace.fire_crackle")),
+				// SoundCategory.AMBIENT, 1F, 1F);
+				// }
+			}
+		}
+	}
+
+	@Override
+	public List<IRecipe> getRecipes(List<IRecipe> recipes) {
+		recipes.add(new ShapedOreRecipe(new ItemStack(this), "SSS", "MDM", "OOO", 'S', Items.STRING, 'M', Blocks.MAGMA,
+				'D', Items.DIAMOND, 'O', Blocks.OBSIDIAN));
+		return recipes;
+	}
+}
