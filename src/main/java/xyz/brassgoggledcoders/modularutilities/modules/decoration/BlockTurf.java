12d36855949e79bdd312ecef28bd571ef25911de
warlordjones <warlordjones@gmail.com>
2018-06-28 00:14:31 +0100

    Add tooltip to turf to explain obtainability
    Reference #13

--- src/main/java/xyz/brassgoggledcoders/modularutilities/modules/decoration/BlockTurf.java
+++ src/main/java/xyz/brassgoggledcoders/modularutilities/modules/decoration/BlockTurf.java
@@ -3,6 +3,7 @@ package xyz.brassgoggledcoders.modularutilities.modules.decoration;
 import java.util.List;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import com.teamacronymcoders.base.blocks.BlockFlat;
 import com.teamacronymcoders.base.util.EnumUtils;
@@ -12,78 +13,89 @@ import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import xyz.brassgoggledcoders.modularutilities.ModularUtilities;
 
 public class BlockTurf extends BlockFlat {
-    public static final PropertyEnum<EnumDirtType> type = PropertyEnum.create("type", EnumDirtType.class);
+	public static final PropertyEnum<EnumDirtType> type = PropertyEnum.create("type", EnumDirtType.class);
 
-    public BlockTurf() {
-        super(Material.GRASS, EnumUtils.getNames(EnumDirtType.class));
-        this.setUnlocalizedName("turf");
-        setDefaultState(this.blockState.getBaseState().withProperty(type, EnumDirtType.NORMAL));
-    }
+	public BlockTurf() {
+		super(Material.GRASS, EnumUtils.getNames(EnumDirtType.class));
+		this.setUnlocalizedName("turf");
+		setDefaultState(this.blockState.getBaseState().withProperty(type, EnumDirtType.NORMAL));
+	}
 
-    @Override
-    @Nonnull
-    @SideOnly(Side.CLIENT)
-    public BlockRenderLayer getBlockLayer() {
-        return BlockRenderLayer.CUTOUT_MIPPED;
-    }
+	@Override
+	@Nonnull
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.CUTOUT_MIPPED;
+	}
 
-    @Override
-    public boolean isOpaqueCube(IBlockState blockState) {
-        return false;
-    }
+	@Override
+	public boolean isOpaqueCube(IBlockState blockState) {
+		return false;
+	}
 
-    @Override
-    public boolean canBlockStay(World worldIn, BlockPos pos) {
-        return !worldIn.isAirBlock(pos.down());
-    }
+	@Override
+	public boolean canBlockStay(World worldIn, BlockPos pos) {
+		return !worldIn.isAirBlock(pos.down());
+	}
 
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        return state.getValue(type).ordinal();
-    }
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return state.getValue(type).ordinal();
+	}
 
-    @Override
-    @Nonnull
-    public BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, type);
-    }
+	@Override
+	@Nonnull
+	public BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, type);
+	}
 
-    @Override
-    @Nonnull
-    @SuppressWarnings("deprecation")
-    public IBlockState getStateFromMeta(int meta) {
-        return getDefaultState().withProperty(type, EnumDirtType.values()[meta]);
-    }
+	@Override
+	@Nonnull
+	@SuppressWarnings("deprecation")
+	public IBlockState getStateFromMeta(int meta) {
+		return getDefaultState().withProperty(type, EnumDirtType.values()[meta]);
+	}
 
-    // @Override
-    // public void getSubBlocks(@Nonnull Item item, CreativeTabs creativeTabs, NonNullList<ItemStack> itemList) {
-    // for(EnumDirtType resourceType : EnumDirtType.values())
-    // itemList.add(new ItemStack(item, 1, resourceType.ordinal()));
-    // }
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, ITooltipFlag advanced) {
+		tooltip.add(I18n.translateToLocal(ModularUtilities.MODID + ".turf.info"));
+	}
 
-    @Override
-    @SideOnly(Side.CLIENT)
-    public List<ModelResourceLocation> getModelResourceLocations(List<ModelResourceLocation> models) {
-        for (EnumDirtType leaveType : EnumDirtType.values()) {
-            models.add(new ModelResourceLocation(getMod().getID() + ":turf", "type=" + leaveType.getName()));
-        }
-        return models;
-    }
+	// @Override
+	// public void getSubBlocks(@Nonnull Item item, CreativeTabs creativeTabs,
+	// NonNullList<ItemStack> itemList) {
+	// for(EnumDirtType resourceType : EnumDirtType.values())
+	// itemList.add(new ItemStack(item, 1, resourceType.ordinal()));
+	// }
 
-    public enum EnumDirtType implements IStringSerializable {
-        NORMAL, DRY, FROZEN, JUNGLE, SWAMP, PODZOL, MYCELIUM;
+	@Override
+	@SideOnly(Side.CLIENT)
+	public List<ModelResourceLocation> getModelResourceLocations(List<ModelResourceLocation> models) {
+		for(EnumDirtType leaveType : EnumDirtType.values()) {
+			models.add(new ModelResourceLocation(getMod().getID() + ":turf", "type=" + leaveType.getName()));
+		}
+		return models;
+	}
 
-        @Override
-        public String getName() {
-            return name().toLowerCase();
-        }
-    }
+	public enum EnumDirtType implements IStringSerializable {
+		NORMAL, DRY, FROZEN, JUNGLE, SWAMP, PODZOL, MYCELIUM;
+
+		@Override
+		public String getName() {
+			return name().toLowerCase();
+		}
+	}
 }
